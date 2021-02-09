ac8935cfa26db7d4744c2c7744e52baa2bf962d9
Yuriy Dolgiy <dolgiy_yum@ekb.sima-land.ru>
2021-02-09 21:29:44 +0500

    +Logger bugfix

--- src/Logger.php
+++ src/Logger.php
@@ -18,17 +18,31 @@ class Logger
   static public function Log($log)
   {
     $dir = dirname(self::$logfile);
+    $dir = realpath($dir);
     if (!is_dir($dir)) {
       if (!mkdir($dir, 0777, true)) {
-        throw new \Exception(sprintf("Не удаётся создать лог фаил: %s.", $dir));
+        echo(sprintf("Не удаётся создать директорию лог фаила: %s.\n", $dir));
+        return false;
       }
     }
-    if (!empty($log) && is_string($log) && !empty(self::$logfile) && $file = fopen(self::$logfile, "a")) {
-      $date_str = date('Y-m-d H:i:s');
-      fwrite($file, "\n" . $date_str . ":  " . $log . "\n");
-      fclose($file);
-     } else {
-      throw new \Exception(sprintf("Не удаётся записать данные: %s.", $logfile));
+    if (!is_writeable($dir) || !is_writable($dir)) {
+      echo(sprintf("Отсутствуют права на запись: %s.\n", self::$logfile));
+      return false;
     }
+    if (!empty($log) && is_string($log) && !empty(self::$logfile)) {
+      if ($file = fopen(self::$logfile, "a")) {
+        chmod(self::$logfile, 0777);
+        $dateStr = date('Y-m-d H:i:s');
+        fwrite($file, "\n" . $dateStr . ":  " . $log . "\n");
+        fclose($file);
+      } else {
+        echo(sprintf("Не удаётся записать лог фаил: %s.\n", $file));
+        return false;
+      }
+    } else {
+      echo "Что-то не так с именем лог файла: ".self::$logfile."\n";
+      return false;
+    }
+    return true;
   }
 }
